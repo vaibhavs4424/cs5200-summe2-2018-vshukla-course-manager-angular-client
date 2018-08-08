029a8c067bedae577cf2d997edb5769b9f99f56a
vaibhavshukla <shukla.v@husky.neu.edu>
2018-08-08 01:52:09 -0400

    Modified root app componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WhiteBoardComponent } from './white-board/white-board.component';
+import { CourseNavigatorServiceClient } from './services/course-navigator.service.client';
+import { CourseServiceClient } from './services/course.service.client';
+import { WidgetServiceClient } from './services/widget.service.client';
+import { UserServiceCleint } from './services/user.service.client';
+import { SectionServiceClient } from './services/section.service.client';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WhiteBoardComponent
+    WhiteBoardComponent,
+    RegisterUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CourseNavigatorServiceClient,
+    CourseServiceClient,
+    WidgetServiceClient,
+    UserServiceCleint,
+    SectionServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
