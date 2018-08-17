5a45aa1f13eb9d30b628d8a0d150de0c1e7436c5
vaibhavshukla <shukla.v@husky.neu.edu>
2018-08-16 23:17:39 -0400

    Integrated Quiz taker component

---
+++ src/app/quiz-taker/quiz-taker.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {QuizServiceClient} from '../services/quiz.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserServiceCleint} from '../services/user.service.client';
+
+@Component({
+  selector: 'app-quiz-taker',
+  templateUrl: './quiz-taker.component.html',
+  styleUrls: ['./quiz-taker.component.css']
+})
+export class QuizTakerComponent implements OnInit {
+
+  constructor(private service: QuizServiceClient,
+              private router: Router,
+              private userService: UserServiceCleint,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  quizId = '';
+  quiz = {};
+  submission = {
+    answers: []
+  };
+
+  submitQuiz = quiz => {
+    this.quiz.submissionTime = new Date();
+    this.service
+      .submitQuiz(quiz)
+      .then(submission => {
+        console.log(submission);
+        this.router.navigate(['/quiz/' + this.quiz._id + '/submission/' + submission._id]);
+      });
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(params => this.service
+        .findQuizById(params['quizId'])
+        .then(quiz => this.quiz = quiz));
+  }
+
+}
