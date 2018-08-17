d0d9a0f1238f1ce0f8bb951626ae1356346a1472
vaibhavshukla <shukla.v@husky.neu.edu>
2018-08-16 23:15:13 -0400

    Integrated fill in the blanska and multiple choice questions

---
+++ src/app/fill-blanks-question/fill-blanks-question.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fill-blanks-question',
+  templateUrl: './fill-blanks-question.component.html',
+  styleUrls: ['./fill-blanks-question.component.css']
+})
+export class FillBlanksQuestionComponent implements OnInit {
+
+  @Input() question;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.question.fillBlanksAnswers = {};
+  }
+
+}
